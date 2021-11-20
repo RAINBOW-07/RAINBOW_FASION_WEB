713ea2efc91f2d950f24b5991814a5bec4f9c132
LeeJungA43 <82315604+LeeJungA43@users.noreply.github.com>
2021-11-21 01:42:09 +0900

    Update Create_Table_Cloth_info.php

--- Create_Table_Cloth_info.php
+++ Create_Table_Cloth_info.php
@@ -6,7 +6,7 @@ if (mysqli_connect_errno()) {
 	printf("Connect failed: %s\n", mysqli_connect_error());
 	exit();
 } else {
-	$sql = "create table Cloth_info (idx INT NOT NULL PRIMARY KEY AUTO_INCREMENT, large_category VARCHAR(20), small_category VARCHAR(20), name VARCHAR(100), price INT, purchase_num VARCHAR(20), site VARCHAR(20), image VARCHAR(100), link VARCHAR(100))";
+	$sql = "create table Cloth_info (idx INT NOT NULL PRIMARY KEY AUTO_INCREMENT, large_category VARCHAR(20), small_category VARCHAR(20), name VARCHAR(100), price INT, purchase_num INT, site VARCHAR(20), image VARCHAR(100), link VARCHAR(100))";
 	$res = mysqli_query($mysqli,$sql);
 	if ($res === TRUE) {
 		echo "Table cloth info successfully created.";
