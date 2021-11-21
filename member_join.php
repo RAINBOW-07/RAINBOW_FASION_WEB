f37e0369049784f2e3c3061764c4b2ea63ccf3fc
LeeJungA43 <82315604+LeeJungA43@users.noreply.github.com>
2021-11-21 16:34:08 +0900

    Update member_join.php

--- member_join.php
+++ member_join.php
@@ -11,4 +11,4 @@ if (mysqli_connect_errno()) {
 
 <meta charset="utf-8" />
 <script type="text/javascript">alert('회원가입이 완료되었습니다.');</script>
-<meta http-equiv="refresh" content="0 url=/home.html">
+<meta http-equiv="refresh" content="0 url=/home.php">
