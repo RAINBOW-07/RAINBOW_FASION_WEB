181ef10723c56355ca4ee91d69c9b3a3c6024677
suu3 <71166763+suu3@users.noreply.github.com>
2021-11-21 19:48:56 +0900

    Update user_leave.php

--- user_leave.php
+++ user_leave.php
@@ -9,20 +9,20 @@
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <body>
 <?php
-    
+    $userid = $_SESSION['user_id'];
     $mysqli = mysqli_connect("127.0.0.1","team07","team07","team07");
     if (mysqli_connect_errno()) {
         printf("Connect failed: %s\n", mysqli_connect_error());
         exit();
     } else {
-        $userid = $_SESSION['user_id'];
+        mysqli_autocommit($mysqli, FALSE);
         $sql = "delete from search_record where id='$userid'"; //id를 참조하고 있는 key 삭제
         $res = mysqli_query($mysqli, $sql);
         $sql = "delete from user_info where id='$userid'";
         $res = mysqli_query($mysqli, $sql);
-    
+        mysqli_commit($mysqli);
+        mysqli_close($mysqli);
     }
-    mysqli_close($mysqli);
 ?>
 <?php
     header('Location: ./logout.php');
